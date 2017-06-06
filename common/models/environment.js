13f58182e330d7b12276fd44dcfe342aaa781ec1
Tai Mai <taimai0604@gmail.com>
2017-06-06 12:55:06 +0700

    function transferToThingSpeak

--- common/models/environment.js
+++ common/models/environment.js
@@ -1,5 +1,35 @@
 'use strict';
+var request = require('request');
+var api_key = "FYQOFWDRQ9JKGBFE";
+module.exports = function (Environment) {
+    //truyen du lieu len thingspeak
+    Environment.transferToThingSpeak = function (data, cb) {
+        if (Object.keys(data).length && !data.id) {// check object null
+            // request server 
+            request.get({ url: "https://api.thingspeak.com/update?api_key="+api_key
+            +"&field1="+data.tempC
+            +"&field2="+data.dewPoint
+            +"&field3="+data.heatIndex
+            +"&field4="+data.humidity
+            +"&field5="+data.pressure
+            +"&field6="+data.lightLevel
+             }, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    cb(null, true);
+                }
+            });
+        } else {
+            cb(null, false);
+        }
+    }
 
-module.exports = function(Environment) {
+    Environment.remoteMethod(
+        'transferToThingSpeak',
+        {
+            http: { path: '/transferToThingSpeak', verb: 'post' },
+            accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
+            returns: { arg: 'result', type: 'boolean' },
+        }
+    );
 
 };
