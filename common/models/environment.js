b6240fcce43967893d6708a2431ce736817c89c3
Tai Mai <taimai0604@gmail.com>
2017-06-18 07:09:06 +0700

    environment

--- common/models/environment.js
+++ common/models/environment.js
@@ -1,26 +1,49 @@
 'use strict';
 
-var Particle = require('particle-api-js');
-var particle = new Particle();
-var token = "3e17a2f5f6c1cd55310c83e62d1546d91d32dac1";
-
 var request = require('request');
-var api_key = "FYQOFWDRQ9JKGBFE";
+var app = require('../../server/server');
+
 module.exports = function (Environment) {
+
+    //handle create Environment
+    Environment.beforeRemote('create', function (context, user, next) {
+        var Device = app.models.Device;
+        Device.findOne({ deviceId: { like: context.args.data.deviceId } }, function (err, device) {
+            if (!err) {
+
+                console.log(context.args.data.deviceId);
+                console.log(device.deviceId);
+                context.args.data.deviceIdParticle = device.deviceId;
+            }
+        });
+
+        next();
+    });
+
     //truyen du lieu len thingspeak
     Environment.transferToThingSpeak = function (data, cb) {
         if (Object.keys(data).length && !data.id) {// check object null
-            // request server 
-            request.get({ url: "https://api.thingspeak.com/update?api_key="+api_key
-            +"&field1="+data.tempC
-            +"&field2="+data.dewPoint
-            +"&field3="+data.heatIndex
-            +"&field4="+data.humidity
-            +"&field5="+data.pressure
-            +"&field6="+data.lightLevel
-             }, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    cb(null, true);
+            var Device = app.models.Device;
+            Device.findOne({where:{deviceId:{like:data.deviceIdParticle}}}, function (err, device) {
+                if (err) {
+                    console.log(data.deviceIdParticle);
+                    var api_key = device.keyThingspeak;
+                    console.log(api_key);
+                    request.get({
+                        url: "https://api.thingspeak.com/update?api_key=" + api_key
+                        + "&field1=" + data.tempC
+                        + "&field2=" + data.dewPoint
+                        + "&field3=" + data.heatIndex
+                        + "&field4=" + data.humidity
+                        + "&field5=" + data.pressure
+                        + "&field6=" + data.lightLevel
+                    }, function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            cb(null, true);
+                        }
+                    });
+                }else{
+                    cb(null, false);
                 }
             });
         } else {
@@ -40,8 +63,8 @@ module.exports = function (Environment) {
     //get data
     Environment.getDataAll = function (filter, cb) {
         console.log(filter);
-        Environment.find({where:filter},function(err,data){
-            cb(null,data);
+        Environment.find({ where: filter }, function (err, data) {
+            cb(null, data);
         });
     }
 
