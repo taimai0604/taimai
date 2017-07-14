06dfc0dedd1989e8fe90d3980a5ff94545b7f454
taima <taimai0604@gmail.com>
2017-07-15 03:45:37 +0700

    fix key thing speak

--- common/models/environment.js
+++ common/models/environment.js
@@ -22,22 +22,23 @@ module.exports = function (Environment) {
             var Device = app.models.Device;
             Device.findOne({ where: { deviceId: { like: data.deviceIdParticle } } }, function (err, device) {
                 if (!err) {
-                    if (device.KeyThingspeak) {
-                        var api_key = device.KeyThingspeak;
-                        request.get({
-                            url: "https://api.thingspeak.com/update?api_key=" + api_key
-                            + "&field1=" + data.tempC
-                            + "&field2=" + data.dewPoint
-                            + "&field3=" + data.heatIndex
-                            + "&field4=" + data.humidity
-                            + "&field5=" + data.pressure
-                            + "&field6=" + data.lightLevel
-                        }, function (error, response, body) {
-                            if (!error && response.statusCode == 200) {
-                                cb(null, true);
-                            }
-                        });
-                    }
+                    console.log("key thing speak "+ device);
+                    // if (device.KeyThingspeak) {
+                        // var api_key = device.KeyThingspeak;
+                        // request.get({
+                        //     url: "https://api.thingspeak.com/update?api_key=" + api_key
+                        //     + "&field1=" + data.tempC
+                        //     + "&field2=" + data.dewPoint
+                        //     + "&field3=" + data.heatIndex
+                        //     + "&field4=" + data.humidity
+                        //     + "&field5=" + data.pressure
+                        //     + "&field6=" + data.lightLevel
+                        // }, function (error, response, body) {
+                        //     if (!error && response.statusCode == 200) {
+                        //         cb(null, true);
+                        //     }
+                        // });
+                    // }
                 } else {
                     cb(null, false);
                 }
